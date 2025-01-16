abb80215a33358ebcd088bcd06882ab9dcdd8192
Voyager6884 <dsr6884voyager@gmail.com>
2025-01-15 16:41:14 -0800

    make swerve module class

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -4,5 +4,17 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.Encoder;
+
 /** Add your docs here. */
-public class SwerveModile {}
+public class SwerveModule {
+    SparkMax m_SteerMotor = new SparkMax(0, MotorType.kBrushless);
+    SparkMax m_DriveMotor = new SparkMax(1, MotorType.kBrushless);
+    SimpleMotorFeedforward m_Drive = new SimpleMotorFeedforward(0, 0);
+    SimpleMotorFeedforward m_Steer = new SimpleMotorFeedforward(1, 0.5);
+    Encoder m_Coder = new Encoder(0 ,0);
+}
