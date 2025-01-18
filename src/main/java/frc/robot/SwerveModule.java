67d4418e1b27782ebe1f24dfeb501dc6f6d2e92e
Voyager6884 <dsr6884voyager@gmail.com>
2025-01-18 13:08:17 -0800

    Have individual Swerve Modules to be used in the SwerveSubsysytem

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -4,26 +4,39 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.Encoder;
 
 /** Add your docs here. */
 public class SwerveModule {
-    SparkMax m_steerMotor = new SparkMax(0, MotorType.kBrushless);
-    SparkMax m_driveMotor = new SparkMax(1, MotorType.kBrushless);
+    SparkMax m_steerMotor;
+    SparkMax m_driveMotor;
+    SparkMaxConfig config;
 
-    CANcoder m_steerAbsoluteEncoder = new CANcoder(0);
-    RelativeEncoder m_steerIntegratedEncoder = m_driveMotor.getEncoder();
+    CANcoder m_steerAbsoluteEncoder;
+    RelativeEncoder m_steerIntegratedEncoder;
 
     PIDController m_PID = new PIDController(0, 0, 0);
+    VelocityVoltage m_voltage = new VelocityVoltage(0);
 
     SimpleMotorFeedforward m_drive = new SimpleMotorFeedforward(0, 0);
     SimpleMotorFeedforward m_steer = new SimpleMotorFeedforward(0, 0);
-    Encoder m_encoder = new Encoder(0,0);
+
+    public SwerveModule(int driveID, int steerID, int encoderID, boolean motorInverted) {
+        m_driveMotor = new SparkMax(driveID, MotorType.kBrushless);
+        m_steerMotor = new SparkMax(steerID, MotorType.kBrushless);
+        config.inverted(motorInverted);
+        m_driveMotor.configure(config, null, null);
+        m_steerMotor.configure(config, null, null);
+
+        m_steerAbsoluteEncoder = new CANcoder(encoderID);
+        m_steerIntegratedEncoder = m_driveMotor.getEncoder();
+    }
 }
--- src/main/java/frc/robot/SwerveSubsystem.java
+++ src/main/java/frc/robot/SwerveSubsystem.java
@@ -7,6 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase {
+  SwerveModule m_FLmodule = new SwerveModule(0, 0, 0, true);
+  SwerveModule m_FRmodule = new SwerveModule(0, 0, 0, true);
+  SwerveModule m_BLmodule = new SwerveModule(0, 0, 0, true);
+  SwerveModule m_BRmodule = new SwerveModule(0, 0, 0, true);
+  
   /** Creates a new SwerveSubsystem. */
   public SwerveSubsystem() {}
 
