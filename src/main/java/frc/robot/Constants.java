849c8d4e5a6553e2a46e9c549da05473111098db
Voyager6884 <dsr6884voyager@gmail.com>
2025-01-18 19:37:41 -0800

    Add Gyro and the Constsants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public final class Constants {
+
+    public static final class swerve {
+        public static final int FL_DRIVE_MOTOR_ID = 0;
+        public static final int FL_STEER_MOTOR_ID = 1;
+        public static final int FR_DRIVE_MOTOR_ID = 3;
+        public static final int FR_STEER_MOTOR_ID = 4;
+        public static final int BL_DRIVE_MOTOR_ID = 5;
+        public static final int BL_STEER_MOTOR_ID = 6;
+        public static final int BR_DRIVE_MOTOR_ID = 7;
+        public static final int BR_STEER_MOTOR_ID = 8;
+    }
+}
--- src/main/java/frc/robot/Subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/SwerveSubsystem.java
@@ -4,17 +4,26 @@
 
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.Pigeon2;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.SwerveModule;
 
 public class SwerveSubsystem extends SubsystemBase {
-    SwerveModule m_FLmodule = new SwerveModule(0, 0, 0, true, 0);
-    SwerveModule m_FRmodule = new SwerveModule(0, 0, 0, true, 0);
-    SwerveModule m_BLmodule = new SwerveModule(0, 0, 0, true, 0);
-    SwerveModule m_BRmodule = new SwerveModule(0, 0, 0, true, 0);
+  SwerveModule m_FLmodule = new SwerveModule(0, 0, 0, true, 0);
+  SwerveModule m_FRmodule = new SwerveModule(0, 0, 0, true, 0);
+  SwerveModule m_BLmodule = new SwerveModule(0, 0, 0, true, 0);
+  SwerveModule m_BRmodule = new SwerveModule(0, 0, 0, true, 0);
+
+  Pigeon2 m_gyro = new Pigeon2(0);
 
   /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {}
+  public SwerveSubsystem() {
+    m_gyro.optimizeBusUtilization();
+    m_gyro.getYaw().setUpdateFrequency(50);
+    m_gyro.getPitch().setUpdateFrequency(50);
+    m_gyro.getRoll().setUpdateFrequency(50);
+  }
 
   @Override
   public void periodic() {
